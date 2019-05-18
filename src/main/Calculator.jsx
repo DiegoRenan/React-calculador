5e3d5f7d495c3e7be10818c4183cdf981f875b1e
DiegoRenan <renan.ccomp@gmail.com>
2019-05-17 23:15:03 -0300

    Second Logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -29,7 +29,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                /* A suit case or if and elses is the better way */
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+               values[0] = this.state.values[0] 
+            }
+            values[1] = 0
+
+            this.setState ({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
@@ -62,7 +84,7 @@ export default class Calculator extends Component {
                 <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
                 <Button label="9" click={this.addDigit}/>
-                <Button label="*" click={this.addDigit} operation/>
+                <Button label="*" click={this.setOperation} operation/>
                 <Button label="4" click={this.addDigit}/>
                 <Button label="5" click={this.addDigit}/>
                 <Button label="6" click={this.addDigit}/>
